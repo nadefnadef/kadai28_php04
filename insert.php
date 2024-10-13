ae55fe8daca0a625848e3032f123fd03429981ba
nadefnadef <comme.un.diamant@gmail.com>
2024-10-13 09:05:24 +0900

    Update insert.php

--- insert.php
+++ insert.php
@@ -14,7 +14,7 @@ if (empty($latitude) || empty($longitude) || empty($duration)) {
 
 // 2. DB接続
 try {
-    $pdo = new PDO('mysql:dbname=nadefnadef_genzaichi01;charset=utf8;host=mysql3101.db.sakura.ne.jp', 'nadefnadef_genzaichi01', 'genzaichi01');
+    $pdo = new PDO('');
 } catch (PDOException $e) {
     exit('DB Connection Error:'.$e->getMessage());
 }
